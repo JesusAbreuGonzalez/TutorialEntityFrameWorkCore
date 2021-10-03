cd8e155eb7e58de692ffab26bc377ff144216a3b
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-10-02 20:26:39 -0400

    Agregando primer ejemplo con la instalación del paquete, establecimiento de la conexión de base de datos y contexto

---
+++ Models/Grade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TutorialEntityFrameWorkCore.Models
+{
+    public partial class Grade
+    {
+        public Grade()
+        {
+            Students = new HashSet<Student>();
+        }
+
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
